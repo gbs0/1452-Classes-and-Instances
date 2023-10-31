fe50668d0dfb847d8773ec574680d308034c39d1
Gabriel Schiavo <gabrielschiavo0@gmail.com>
2023-10-31 20:16:43 -0300

    automated commit

--- 02-car.rb
+++ 02-car.rb
@@ -13,8 +13,8 @@ class Car
 
   def generate_plate
     # Ao chamar o método, deveremos reescrever o valor de @plate,
-    # p/ receber um conjunto de 3 LEtras e 4 digitos aleatórios separados por hífen
-    # LRF-9105
+    # p/ receber um conjunto de 3 Letras e 4 dígitos aleatórios separados por hífen
+    # Ex. LRB-9105
     letters = ("A".."Z").to_a.shuffle.sample(3).join
     digits = ("0".."9").to_a.shuffle.sample(4).join
     @plate = "#{letters}-#{digits}"
